75a1f8937409aadb83efa40fe39c27acd2f04323
Marcos <marcos.alloza4@gmail.com>
2023-05-31 17:59:29 +0200

    cambios

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { CartService } from 'src/app/shared/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  
+  isCartaPage: boolean = false;
+  badgeCount: number = 0;
+
+  constructor(private router: Router, private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isCartaPage = event.url.includes('/carta');
+      }
+    });
+  }
+
+  get cartItemsCount(): number {
+    return this.cartService.cartItemCount;
+  }
+
 }

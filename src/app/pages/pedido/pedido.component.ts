7f40b5e15b341060c20f58ed7e8c193b1c4662aa
gonzsanz <davidgonzsanz02@gmail.com>
2023-06-03 21:08:08 +0200

    obtener id y obtener sus pedidos

--- src/app/pages/pedido/pedido.component.ts
+++ src/app/pages/pedido/pedido.component.ts
@@ -3,6 +3,7 @@ import { Pedido } from 'src/app/shared/interfaces/pedido';
 import { PedidoService } from 'src/app/shared/services/pedido.service';
 import { format } from 'date-fns';
 import { es } from 'date-fns/locale';
+import { UserService } from 'src/app/shared/services/user.service';
 
 interface Detalles {
   cantidad: number;
@@ -15,6 +16,7 @@ interface Detalles {
   styleUrls: ['./pedido.component.scss'],
 })
 export class PedidoComponent {
+  id: number | undefined;
   pedidos: Pedido[] = [];
   detalles: Detalles[] = [
     {
@@ -27,20 +29,41 @@ export class PedidoComponent {
     },
   ];
 
-  constructor(private pedidoService: PedidoService) {}
+  constructor(
+    private pedidoService: PedidoService,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
     this.obtenerPedidos();
   }
 
-  obtenerPedidos() {
-    this.pedidoService.getOrderByUser(1).subscribe((pedidos) => {
-      this.pedidos = pedidos.map((pedido: { date: string | number | Date }) => {
-        const fechaFormateada = format(new Date(pedido.date), 'd MMMM yyyy', {
-          locale: es,
-        });
-        return { ...pedido, date: fechaFormateada };
+  async obtenerPedidos() {
+    this.id = await this.getUsuario();
+    if (this.id) {
+      this.pedidoService.getOrderByUser(this.id).subscribe((pedidos) => {
+        this.pedidos = pedidos.map(
+          (pedido: { date: string | number | Date }) => {
+            const fechaFormateada = format(
+              new Date(pedido.date),
+              'd MMMM yyyy',
+              {
+                locale: es,
+              }
+            );
+            return { ...pedido, date: fechaFormateada };
+          }
+        );
       });
-    });
+    }
+  }
+
+  private async getUsuario(): Promise<number | undefined> {
+    const email = sessionStorage.getItem('email');
+    if (email) {
+      const usuario = await this.userService.getUser(email).toPromise();
+      return usuario.user_id;
+    }
+    return undefined;
   }
 }

c6db323f681d0375adb142929bd15144ad72f48a
Marcos <marcos.alloza4@gmail.com>
2023-06-05 00:49:09 +0200

    cart improves and saved in session storage

--- src/app/pages/carrito/carrito.component.ts
+++ src/app/pages/carrito/carrito.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Product } from 'src/app/shared/interfaces/product';
 import { CartService } from 'src/app/shared/services/cart.service';
+import { StorageService } from 'src/app/shared/services/storage.service';
 
 @Component({
   selector: 'app-carrito',
@@ -14,14 +15,33 @@ export class CarritoComponent {
   constructor(
     public dialogRef: MatDialogRef<CarritoComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Product,
-    private cartService: CartService
+    private cartService: CartService,
+    private storageService: StorageService
   ) {}
+
+  ngOnInit(): void {
+    if (this.storageService.existsCart()) {
+      this.storageService.getCart();
+    }
+    this.cart = this.cartService.getItems();
+  }
 
   close(): void {
     this.dialogRef.close();
   }
-  
+
   getQuantity(productId: number): number {
     return this.cartService.getQuantity(productId);
+  }
+
+  removeItem(i: number): void {
+    this.cartService.removeItem(i);
+    setTimeout(() => {
+      this.cart = this.cartService.getItems();
+    }, 0);
+  }
+
+  getTotal(): number {
+    return this.cartService.getTotal();
   }
 }
--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { Product } from '../interfaces/product';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,27 +10,80 @@ export class CartService {
   cartItemCount = 0;
   cartQuantity = 0;
 
-  items: Product[] = [];
-  quantities: Map<number, number> = new Map<number, number>();
+  items: { product: Product; quantity: number }[] = [];
 
-  constructor() {}
+  constructor(private storageService: StorageService) {
+    this.loadCart();
+  }
+
+  // Guardar el carrito en el localStorage
+  private saveCart(): void {
+    this.storageService.setCart(this.items);
+  }
+
+  // Cargar el carrito desde el localStorage al iniciar la aplicación
+  private loadCart(): void {
+    if (this.storageService.existsCart()) {
+      this.items = this.storageService.getCart();
+      this.cartItemCount = this.items.length;
+    }
+  }
 
   // Guardar la cantidad de productos que se agregan al carrito -- unitario
-  addItem(): void {
-    this.cartItemCount++;
+  addItem(product: Product): void {
+    const existingItem = this.items.find(
+      (item) => item.product.product_id === product.product_id
+    );
+
+    if (!existingItem) {
+      this.cartItemCount++;
+    }
   }
 
   addToCart(product: Product, quantity: number): void {
-    this.addItem();
-    this.items.push(product);
-    this.quantities.set(product.product_id, quantity);
+    this.addItem(product);
+    const existingItem = this.items.find(
+      (item) => item.product.product_id === product.product_id
+    );
+
+    if (existingItem) {
+      existingItem.quantity += quantity; // Actualizar cantidad si el producto ya está en el carrito
+    } else {
+      this.items.push({ product, quantity }); // Agregar nuevo producto al carrito
+    }
+    this.saveCart();
   }
 
   getItems(): Product[] {
-    return this.items;
+    const cartData = this.storageService.getCart();
+    this.items = cartData.map((item) => ({
+      product: item.product,
+      quantity: item.quantity,
+    }));
+    return this.items.map((item) => item.product);
   }
 
   getQuantity(productId: number): number {
-    return this.quantities.get(productId) || 0;
+    const item = this.items.find(
+      (item) => item.product.product_id === productId
+    );
+    return item ? item.quantity : 0;
+  }
+
+  removeItem(i: number): void {
+    if (this.items[i].quantity > 1) {
+      this.items[i].quantity--;
+    } else {
+      this.items.splice(i, 1);
+      this.cartItemCount--;
+    }
+    this.saveCart();
+  }
+
+  getTotal(): number {
+    return this.items.reduce(
+      (total, item) => total + item.product.price * item.quantity,
+      0
+    );
   }
 }

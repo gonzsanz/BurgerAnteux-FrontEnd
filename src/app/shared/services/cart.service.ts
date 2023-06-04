f03b91c53804ecdf180952a06429a1744e1db9c2
gonzsanz <davidgonzsanz02@gmail.com>
2023-06-04 14:40:05 +0200

    funciona!!

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -3,40 +3,34 @@ import { Observable, Subject } from 'rxjs';
 import { Product } from '../interfaces/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
   cartItemCount = 0;
-  quantity = 1;
   cartQuantity = 0;
-  
-  cart: { product: Product, quantity: number }[] = [];
 
-  private cartUpdated = new Subject<{ product: Product, quantity: number }[]>();
+  items: Product[] = [];
+  quantities: Map<number, number> = new Map<number, number>();
 
+  constructor() {}
 
-  constructor() { }
-
-addToCart(product: Product, quantity: number): void {
-    const existingItem = this.cart.find(item => item.product.product_id === product.product_id);
-    if (existingItem) {
-      existingItem.quantity += quantity;
-    } else {
-      this.cart.push({ product, quantity });
-    }
-    this.cartUpdated.next(this.cart);
-  }
-
-  getCart(): Observable<{ product: Product, quantity: number }[]> {
-    return this.cartUpdated.asObservable();
-  }
-
-
-  //guardar la cantidad de productos que se agregan al carrito -- unitario
+  // Guardar la cantidad de productos que se agregan al carrito -- unitario
   addItem(): void {
     this.cartItemCount++;
   }
 
+  addToCart(product: Product, quantity: number): void {
+    this.addItem();
+    this.items.push(product);
+    this.quantities.set(product.product_id, quantity);
 
+  }
 
+  getItems(): Product[] {
+    return this.items;
+  }
+
+  getQuantity(productId: number): number {
+    return this.quantities.get(productId) || 0;
+  }
 }
